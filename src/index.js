3e54bd05f5f8d03d70fefdcc1cd97a93001458b7
VLeshkevich <leshkevich.veronica@gmail.com>
2018-03-05 17:15:58 +0300

    feature/additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	if (str.length%2 != 0) return false;
+	var stack = [];
+	for(var i = 0; i < str.length; i++){
+		for(var j=0; j<bracketsConfig.length; j++){
+			if (str[i] == bracketsConfig[j][1] && stack[stack.length - 1] == bracketsConfig[j][0])
+				stack.pop();
+			else if (str[i] == bracketsConfig[j][0])
+				stack.push(str[i]);
+			}
+	}
+	if(stack.length == 0){
+		return true;
+	}
+		else return false;
+	}
